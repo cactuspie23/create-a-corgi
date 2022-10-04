a789c52a1b9bdcc49eeffffe9e14c30cf47bd03f
AlyssaD <pink_lime@live.com>
2022-10-04 19:34:47 -0400

    add favicon and styling to corgi index

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Create-A-Corgi
+
+
+
+<a href="https://www.freepik.com/free-vector/v60-pla-kala-03-animals_3129737.htm#page=2&query=corgi%20icon&position=17&from_view=search&track=sph">Favicon Image by rawpixel.com</a> on Freepik
--- models/corgi.js
+++ models/corgi.js
@@ -17,7 +17,7 @@ const corgiSchema = new Schema({
   },
   color: {
     type: String,
-    enum: ['Red and White', 'Black and White', 'Tri', 'Blue Merle'],
+    enum: ['Red and White', 'Black and White', 'Tri', 'Sable'],
   },
   age: {
     type: Number,
